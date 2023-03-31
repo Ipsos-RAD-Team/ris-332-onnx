3dab8e4d484b9a42fc583e615cb20e56c976145a
samuel.stevens <samuel.stevens@ipsos.com>
2023-03-31 21:10:03 +0000

    [RIS-332] initial batch tester working

--- batch_test.py
+++ batch_test.py
@@ -3,72 +3,55 @@ import transformers
 import onnx
 import onnxruntime
 import numpy as np
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
 from sklearn.metrics.pairwise import cosine_similarity
 from time import perf_counter
 from tqdm import tqdm
 
-def batch_tokens(tokens: Dict[str, torch.Tensor], batch_size: int) -> Tuple[torch.Tensor]:
+def batch_tokens(tokens: Dict[str, torch.Tensor], batch_size: int, labeled: bool = False) -> Union[Tuple[torch.Tensor], Dict[str, torch.Tensor]]:
     """Splits the token tensors into batches of given batch size.
     
     Args:
         tokens (Dict[str, torch.Tensor]): A dictionary containing token tensors.
         batch_size (int): The batch size to split the tensors into.
+        labeled (bool): A boolean flag indicating whether to label the batched tensors or not. Default is False.
     
     Returns:
-        Tuple[torch.Tensor]: A tuple of batched tensor values.
+        Union[Tuple[torch.Tensor], Dict[str, torch.Tensor]]: 
+            - If labeled=False, a tuple of batched tensor values.
+            - If labeled=True, a dictionary containing labeled batched tensor values.
     """
+    num_sentences = tokens['input_ids'].shape[0]
     split_tensors = {}
     for key, value in tokens.items():
         if isinstance(value, torch.Tensor):
             repeated_value = value.repeat(batch_size, *[1] * (value.ndim - 1))
-            split_tensors[key] = torch.split(repeated_value, batch_size)
-    batched_tensors = {}
-    for key, tensors in split_tensors.items():
-        batched_tensors[key] = torch.cat(tensors, dim=0)
-    return tuple(batched_tensors.values())
-
-def batch_tokens_onnx(tokens: Dict[str, torch.Tensor], batch_size: int) -> List[Dict[str, torch.Tensor]]:
-    """Splits the token tensors into batches of given batch size.
-
-    Args:
-        tokens (Dict[str, torch.Tensor]): A dictionary containing token tensors.
-        batch_size (int): The batch size to split the tensors into.
-
-    Returns:
-        List[Dict[str, torch.Tensor]]: A list of dictionaries containing batched tensor values.
-    """
-    num_samples = tokens['input_ids'].shape[0]
-    num_batches = (num_samples + batch_size - 1) // batch_size
-    batched_tokens = []
-    for i in range(num_batches):
-        start_index = i * batch_size
-        end_index = min((i + 1) * batch_size, num_samples)
-        batch_tokens = {
-            key: value[start_index:end_index] for key, value in tokens.items()
-        }
-        batched_tokens.append(batch_tokens)
-    return batched_tokens
-
-
-
-
-
-
+            tensors = torch.split(repeated_value, batch_size * num_sentences)
+            batched_tensor = torch.cat(tensors, dim=0)[:num_sentences]
+            if labeled:
+                split_tensors[key] = batched_tensor
+            else:
+                split_tensors[key] = batched_tensor.view(-1)
+    if labeled:
+        return split_tensors
+    else:
+        return tuple(split_tensors.values())
 
 # Load the XLM-Roberta base model
-model_name = 'xlm-roberta-large'
+model_name = 'xlm-roberta-large-finetuned-conll03-english'
 tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
 model = transformers.AutoModel.from_pretrained(
     model_name, 
     # return_dict=False
     )
 
-sentence = ["Alya told Jasmine that Andrew could pay with cash."]
-
+sentences = [
+    "Alya told Jasmine that Andrew could pay with cash.",
+    "The quick brown fox jumps over the lazy dog."
+]
 
 tokens = tokenizer.batch_encode_plus(
-    sentence, 
+    sentences, 
     max_length=128,
     padding='max_length', 
     truncation=True, 
@@ -77,58 +60,50 @@ tokens = tokenizer.batch_encode_plus(
 
 input_names= list(tokens.keys())
 print(input_names)
-example_inputs = batch_tokens(tokens, 1)
+BATCH_SIZE = 2
+example_inputs = batch_tokens(tokens, BATCH_SIZE, labeled=True)
+print(example_inputs)
 model.eval()
-runs = 30
+runs = 5
 with torch.no_grad():
     start_time = perf_counter()
     for i in tqdm(range(runs)):
-        output1 = (model(*example_inputs)[0].mean(dim=1))
+        output1 = (model(**example_inputs)[0].mean(dim=1))
     stop_time = perf_counter()
-    print(f"#1 Throughput for {runs} was {runs/(stop_time-start_time)}")
-    # print(output1)
-    # print(model(*example_inputs)[0].mean(dim=1))
-    # output_names = list(model(**tokens).keys())
-    
-# exit()
+    print(f"#1 Throughput for {runs} was {BATCH_SIZE*runs/(stop_time-start_time)}")
+
+    output_names = list(model(**tokens).keys())
+print(model(**example_inputs)[0])
+
+
 onnx_file = './model/model.onnx'
 # torch.onnx.export(
 #                 model,
 #                 example_inputs,
-#                 export_params=True,
-#                 opset_version=11,
+#                 export_params=True, # without the extra files the model will not load
+#                 opset_version=11,   # ver 11+ required for several modern op types in the model
 #                 do_constant_folding=True,
 #                 input_names=input_names,
 #                 output_names=output_names,
-#                 f=onnx_file
+#                 f=onnx_file,
 # )
-
-# onnx_model = onnx.load(onnx_file)
-
+# exit()
 import numpy as np
 import onnxruntime
+print('loading model')
 ort_session = onnxruntime.InferenceSession(onnx_file)
-
 start_time = perf_counter()
+
+input_numpy = {
+    'input_ids': example_inputs['input_ids'].numpy(),
+    'attention_mask': example_inputs['attention_mask'].numpy()
+}
+output_dict = {}
+
 for i in tqdm(range(runs)):
-    outputs = ort_session.run(None, {
-        "input_ids"     : tokens['input_ids'].numpy(), 
-        'attention_mask': tokens['attention_mask'].numpy()
-        }
+    outputs = ort_session.run(output_names, 
+        input_numpy
         )
+    
 stop_time = perf_counter()
-print(f"#2 Throughput for {runs} was {runs/(stop_time-start_time)}")
-
-# exit()
-# Convert output tensor to PyTorch tensor
-output_tensor = torch.from_numpy(outputs[0])
-
-# Compute mean sentence embedding
-output2 = output_tensor.mean(dim=1)
-
-# Print the mean sentence embedding
-print(output2)
-
-similarity = cosine_similarity(output1, output2)
-print('Similarity is:', similarity)
-
+print(f"#2 Throughput for {runs} was {BATCH_SIZE*runs/(stop_time-start_time)}")
---
+++ in_progress.py
@@ -0,0 +1,104 @@
+import torch
+import transformers
+import onnx
+import onnxruntime
+import numpy as np
+from typing import Dict, List, Tuple
+from sklearn.metrics.pairwise import cosine_similarity
+from time import perf_counter
+from tqdm import tqdm
+
+def batch_tokens(tokens: Dict[str, torch.Tensor], batch_size: int) -> Tuple[torch.Tensor]:
+    """Splits the token tensors into batches of given batch size.
+    
+    Args:
+        tokens (Dict[str, torch.Tensor]): A dictionary containing token tensors.
+        batch_size (int): The batch size to split the tensors into.
+    
+    Returns:
+        Tuple[torch.Tensor]: A tuple of batched tensor values.
+    """
+    split_tensors = {}
+    for key, value in tokens.items():
+        if isinstance(value, torch.Tensor):
+            repeated_value = value.repeat(batch_size, *[1] * (value.ndim - 1))
+            split_tensors[key] = torch.split(repeated_value, batch_size)
+    batched_tensors = {}
+    for key, tensors in split_tensors.items():
+        batched_tensors[key] = torch.cat(tensors, dim=0)
+    return tuple(batched_tensors.values())
+
+# Load the XLM-Roberta base model
+model_name = 'xlm-roberta-large'
+tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+model = transformers.AutoModel.from_pretrained(
+    model_name, 
+    # return_dict=False
+    )
+
+sentence = ["Alya told Jasmine that Andrew could pay with cash."]
+
+
+tokens = tokenizer.batch_encode_plus(
+    sentence, 
+    max_length=128,
+    padding='max_length', 
+    truncation=True, 
+    return_tensors="pt"
+)
+
+input_names= list(tokens.keys())
+print(input_names)
+example_inputs = batch_tokens(tokens, 1)
+model.eval()
+runs = 30
+with torch.no_grad():
+    start_time = perf_counter()
+    for i in tqdm(range(runs)):
+        output1 = (model(*example_inputs)[0].mean(dim=1))
+    stop_time = perf_counter()
+    print(f"#1 Throughput for {runs} was {runs/(stop_time-start_time)}")
+
+    output_names = list(model(**tokens).keys())
+    
+# exit()
+onnx_file = './model/model.onnx'
+torch.onnx.export(
+                model,
+                example_inputs,
+                export_params=True, # without the extra files the model will not load
+                opset_version=11,   # ver 11+ required for several modern op types in the model
+                do_constant_folding=True,
+                input_names=input_names,
+                output_names=output_names,
+                f=onnx_file,
+)
+
+import numpy as np
+import onnxruntime
+print('loading model')
+ort_session = onnxruntime.InferenceSession(onnx_file)
+
+start_time = perf_counter()
+for i in tqdm(range(runs)):
+    outputs = ort_session.run(None, {
+        "input_ids"     : tokens['input_ids'].numpy(), 
+        'attention_mask': tokens['attention_mask'].numpy()
+        }
+        )
+stop_time = perf_counter()
+print(f"#2 Throughput for {runs} was {runs/(stop_time-start_time)}")
+
+# Convert output tensor to PyTorch tensor
+output_tensor = torch.from_numpy(outputs[0])
+
+# Compute mean sentence embedding
+output2 = output_tensor.mean(dim=1)
+
+# Print the mean sentence embedding
+print('Normal tensor', output1)
+print('Onnx tensor', output2)
+
+similarity = cosine_similarity(output1, output2)
+print('Similarity is:', similarity)
+
