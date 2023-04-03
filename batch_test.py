b5692475947e4ff97176376fe80cdad31d86de55
samuel.stevens <samuel.stevens@ipsos.com>
2023-04-03 13:25:18 +0000

    [RIS-332] + batched onnx working w/ throughput metrics

--- batch_test.py
+++ batch_test.py
@@ -7,6 +7,8 @@ from typing import Dict, List, Tuple, Union
 from sklearn.metrics.pairwise import cosine_similarity
 from time import perf_counter
 from tqdm import tqdm
+import numpy as np
+import onnxruntime
 
 def batch_tokens(tokens: Dict[str, torch.Tensor], batch_size: int, labeled: bool = False) -> Union[Tuple[torch.Tensor], Dict[str, torch.Tensor]]:
     """Splits the token tensors into batches of given batch size.
@@ -59,51 +61,63 @@ tokens = tokenizer.batch_encode_plus(
 )
 
 input_names= list(tokens.keys())
-print(input_names)
-BATCH_SIZE = 2
+BATCH_SIZE = 3
 example_inputs = batch_tokens(tokens, BATCH_SIZE, labeled=True)
-print(example_inputs)
 model.eval()
 runs = 5
+
+# throughput testing
 with torch.no_grad():
     start_time = perf_counter()
     for i in tqdm(range(runs)):
-        output1 = (model(**example_inputs)[0].mean(dim=1))
+        rawoutput1 = model(**example_inputs)
+        output1 = (rawoutput1[0].mean(dim=1))
     stop_time = perf_counter()
     print(f"#1 Throughput for {runs} was {BATCH_SIZE*runs/(stop_time-start_time)}")
 
+    # output tensor key names used by ONNX
     output_names = list(model(**tokens).keys())
-print(model(**example_inputs)[0])
 
+print(f"Embedding output from model using no compiler:\n{output1}")
 
 onnx_file = './model/model.onnx'
-# torch.onnx.export(
-#                 model,
-#                 example_inputs,
-#                 export_params=True, # without the extra files the model will not load
-#                 opset_version=11,   # ver 11+ required for several modern op types in the model
-#                 do_constant_folding=True,
-#                 input_names=input_names,
-#                 output_names=output_names,
-#                 f=onnx_file,
-# )
-# exit()
-import numpy as np
-import onnxruntime
+# model compile for ONNX
+
+torch.onnx.export(
+                model,
+                example_inputs,
+                export_params=True, # without the extra files the model will not load
+                opset_version=11,   # ver 11+ required for several modern op types in the model
+                do_constant_folding=True,
+                input_names=input_names,
+                output_names=output_names,
+                f=onnx_file,
+)
+
+
+
 print('loading model')
 ort_session = onnxruntime.InferenceSession(onnx_file)
 start_time = perf_counter()
 
+# inputs need to be reformatted for ONNX
 input_numpy = {
     'input_ids': example_inputs['input_ids'].numpy(),
     'attention_mask': example_inputs['attention_mask'].numpy()
 }
 output_dict = {}
 
+# throughput testing for ONNX
 for i in tqdm(range(runs)):
-    outputs = ort_session.run(output_names, 
+    output2 = ort_session.run(output_names, 
         input_numpy
         )
     
+# reconstructing the ONNX numpy output back to tensors
+tensor_list = [torch.from_numpy(arr) for arr in output2]
+tensor_list[1] = tensor_list[1].unsqueeze(1)
+combined_tensor = torch.cat(tensor_list, dim=1)
+
 stop_time = perf_counter()
 print(f"#2 Throughput for {runs} was {BATCH_SIZE*runs/(stop_time-start_time)}")
+print(combined_tensor.mean(dim=1))
